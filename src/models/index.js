b891c4bfebe766708219dda99630a54908cb0f9c
incredible92 <sulymanabdullahi@gmail.com>
2020-11-13 17:33:51 +0100

    feet: installation

--- src/models/index.js
+++ src/models/index.js
@@ -1,36 +1,36 @@
-// // import { model, Schema } from 'mongoose'
-// import { hashPassword } from '../utils'
+// import { model, Schema } from 'mongoose'
+import { hashPassword } from '../utils'
 
-// const anonSchema = new Schema({
-//   name: String,
-//   username: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//     index: true
-//   },
-//   password: {
-//     type: String,
-//     required: true
-//   },
-//   email: String,
-//   messages: [{
-//     text: String,
-//     createdAt: {
-//       type: Date,
-//       default: Date.now
-//     }
-//   }]
-// }, { timestamps: true })
+const anonSchema = new Schema({
+  name: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: String,
+  messages: [{
+    text: String,
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }]
+}, { timestamps: true })
 
-// anonSchema.pre('save', function (next) {
-//   const anon = this
+anonSchema.pre('save', function (next) {
+  const anon = this
 
-//   if (anon.isModified('password')) {
-//     anon.password = hashPassword(anon.password)
-//   }
+  if (anon.isModified('password')) {
+    anon.password = hashPassword(anon.password)
+  }
 
-//   next()
-// })
+  next()
+})
 
-// export default model('Anon', anonSchema)
+export default model('Anon', anonSchema)
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
-// import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 
-// dotenv.config()
+dotenv.config()
 
 export const tokenSecret = process.env.TOKEN_SECRET
 export const dbURL = process.env.DB_URL
