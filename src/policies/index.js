0cfa28b6a38294f65fbec320523a09ebb3f01cfd
incredible92 <sulymanabdullahi@gmail.com>
2020-11-14 10:18:46 +0100

    feet: registerPolicy

--- src/policies/index.js
+++ src/policies/index.js
@@ -13,3 +13,17 @@ export const loginPolicy = (req, res, next) => {
 
   next()
 }
+
+export const registerPolicy = (req, res, next) => {
+  const schema = Joi.object({
+    username: Joi.string().alphanum().min(2).required(),
+    email: Joi.string().email(),
+    password: Joi.string().min(5).required()
+  })
+
+  const { error } = schema.validate(req.body)
+
+  if (error) return serverResponse(res, error.details[0].message, 400)
+
+  next()
+}
