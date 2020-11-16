1e9c9b8711ca806c26010ca383b54ed2011e267c
incredible92 <sulymanabdullahi@gmail.com>
2020-11-16 12:57:16 +0100

    feet: router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { loginPolicy } from '../policies'
-import { login } from '../services/auth.services'
+import { loginPolicy, registerPolicy} from '../policies'
+import { login, register } from '../services/auth.services'
 import { serverResponse } from '../utils'
 const router = Router()
 
 router.post('/login', loginPolicy, login)
+router.post('/register', registerPolicy, register)
 
 router.get('/', (req, res) => {
   serverResponse(res, 'API works!')
